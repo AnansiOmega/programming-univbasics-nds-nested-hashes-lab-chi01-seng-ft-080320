192618de9a7e2fbc3520a9fb0f00f40a086e1355
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-19 16:18:51 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,8 +11,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+
 end
 
 def aoh_lookup(aoh, row, key)
@@ -22,3 +21,6 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
+
+  # Using Array literal syntax only, build a nested array that uses the data in
+  # held in the constants
